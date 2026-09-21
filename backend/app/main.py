ec1e7b8d48b43cc4fded0659a2a7f2a2fa13fcc7
Santanu <prasantanu00@gmail.com>
2026-09-21 09:43:20 +0530

    Allow Railway frontend CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -45,7 +45,10 @@ app = FastAPI(
 # Frontend to Backend API access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173",
+                   "http://127.0.0.1:5173",
+                   "https://commoditytrack-production-2084.up.railway.app",
+                   ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
